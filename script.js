733f4f17c68d2552a93e313f664469d419e1f3cd
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-01 15:12:12 +0330

    js code

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let browser=document.getElementById('browser');
 let user=navigator.userAgent;
 
-
+if(user.match(/firefox/i)){
+    browser.innerHTML='شما از مرورگر فایرفاکس استفاده می کنید.';
+}else if(user.match(/chrome/i)){
+    browser.innerHTML='شما از مرورگر کروم استفاده می کنید.';
+}else{
+    browser.innerHTML='شما از مرورگری خارج از موارد تعیین شده استفاده می کنید.';
+}
